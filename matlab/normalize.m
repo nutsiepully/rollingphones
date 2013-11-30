daff2d5ec7f7e9800186e1e09bfc35595a4de972
Alejandro Carbonara <plothole@gmail.com>
2013-11-30 17:32:17 -0500

    Fixed broken normalization

--- matlab/normalize.m
+++ matlab/normalize.m
@@ -1,7 +1,8 @@
 function [ norm_mat ] = normalize( mat )
     norm_mat = bsxfun(@minus, mat, min(mat));              % 0 - max
-    if(max(norm_mat)~=0)
-       norm_mat = bsxfun(@rdivide, norm_mat, max(norm_mat));  % 0 - 1 scaling
-       norm_mat = norm_mat .* 2 - 1;                          % -1 - 1 scaling
-    end
+    maxCol = max(norm_mat);
+    maxCol(~maxCol) = 1;    %Replace all 0s with 1s (to prevent /0)
+    norm_mat = bsxfun(@rdivide, norm_mat, maxCol);  % 0 - 1 scaling
+    norm_mat = norm_mat .* 2 - 1;                          % -1 - 1 scaling
+    %end
 end
