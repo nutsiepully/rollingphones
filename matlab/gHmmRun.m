750995220956e2ac55978a4132447abf251fca28
Pulkit Bhuwalka <pulkit.bosco05@gmail.com>
2013-12-01 21:38:48 -0500

    Putting in the missing prior generation.

--- matlab/gHmmRun.m
+++ matlab/gHmmRun.m
@@ -1,10 +1,14 @@
 
-[ initState, transmat, mu, sigma ] = gausshmm_train_observed(Xtrain', Ytrain', 6);
+Q = 6;          % Number of states 
 
-B = mixgauss_prob(Xtest', mu, sigma, ones(6, 1));
+%train
+prior = normalise(rand(Q,1));
+[ initState, transmat, mu, sigma ] = gausshmm_train_observed(Xtrain', Ytrain', Q);
 
+% predict
+B = mixgauss_prob(Xtest', mu, sigma, ones(Q, 1));
 [path] = viterbi_path(prior, transmat, B);
 
+% verify
 res = (path' == Ytest);
-
 display(length(res == 1) / length(Ytest) * 100);
